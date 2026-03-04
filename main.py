d45b9645fd38cb499e90d0f84399f0b3e06b6964
Maret Vaabel <maret.vaabel@voco.ee>
2026-03-04 10:22:24 +0000

    added backGround image

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ os.environ.setdefault("SDL_AUDIODRIVER", "dummy")
 pygame.init()
 screen = pygame.display.set_mode((640, 360))
 pygame.display.set_caption("Minu Pygame pilves")
+backgroundImage = pygame.image.load("taust.png")
 
 clock = pygame.time.Clock()
 running = True
@@ -16,8 +17,9 @@ while running:
         if e.type == pygame.QUIT:
             running = False
 
-    screen.fill((30, 30, 30)) # Tumehall taust
-    pygame.draw.circle(screen, (80, 200, 120), (320, 180), 60) # Roheline ring
+    #screen.fill((30, 30, 30)) # Tumehall taust
+   # pygame.draw.circle(screen, (80, 200, 120), (320, 180), 60) # Roheline ring
+    screen.blit(backgroundImage, (0, 0))
     pygame.display.flip()
     clock.tick(60)
 
